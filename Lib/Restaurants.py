3e95ff0cf4cd44d39d06e444f73fb82b85195462
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2023-08-28 00:30:50 +0300

    made changes to the review file

--- Lib/Restaurants.py
+++ Lib/Restaurants.py
@@ -5,14 +5,17 @@ class restaurant:
     
     # add a decorator to make the restaurant name read only
     # @property
-    # def restaurant_name(self):
-    #     return self._restaurant_name
+    def restaurant_name(self):
+        return self._restaurant_name
     
     # returns a string of the restaurant name instead object
     def __str__(self):
         return f"Restaurant Name: {self.restaurant_name}"
     
 restaurant1 = restaurant("Hotel Bilha")
+restaurant2 = restaurant("The mark")
+restaurant3 = restaurant("Villa Rosa Kempinski")
+
 
 # testing to see if the @property decorator works
 # restaurant1.restaurant_name = "New restaurant"
--- Lib/Review.py
+++ Lib/Review.py
@@ -1,20 +1,28 @@
 from Customer import customer
 from Restaurants import restaurant
 
-# from Customer import customer1
-# from Restaurants import restaurant1
-
-# from Customer import customer2
+from Customer import customer1
+from Restaurants import restaurant1
+from Restaurants import restaurant3
 
 class review:
+    all_reviews = []
     def __init__ (self, customer, restaurant, rating):
         self.customer = customer
         self.restaurant = restaurant
         self.rating = rating
+        review.all_reviews.append(self)
 
     def __str__(self):
         return f"Customer review: {self.customer}, {self.restaurant}, Rating: {self.rating}"
 
-review1 = review(customer, restaurant, 4.5)
+review1 = review(customer, restaurant1, 4.5)
+review1 = review(customer1, restaurant3, 5)
 
-print(review1)
+
+# iterating through the all_review list to get all the reviews 
+for review in review.all_reviews:
+    print(review)
+
+# printing a single review
+# print(review1)

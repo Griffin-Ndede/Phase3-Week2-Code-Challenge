876e7372a0aeecb58741bc6ae140a9bae7a78e7d
Griffin-Ndede <griffin.omondi@students.moringaschool.com>
2023-08-27 21:28:32 +0300

    created customer class and initialized it

---
+++ Lib/Customer.py
@@ -0,0 +1,6 @@
+class customer:
+    def __init__(self, first_name, last_name, family_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.family_name = family_name
+        
---
+++ Lib/Restaurants.py
@@ -0,0 +1,7 @@
+from Customer import customer
+
+class restaurant:
+    def __init__(self, restaurant_name, customer):
+        self.restaurant_name = restaurant_name
+
+first.name
